04643940066485d3302967bfda30680bb7187bf7
jonmcelroy <jonmcelroy@73532c6c-3e55-11de-be05-5f7a86268029>
2009-05-19 16:20:39 +0000

    This is the beginning of the gui for the server side.

---
+++ server/src/com/pineapple_gui/ServerGUI.java
@@ -0,0 +1,31 @@
+import javax.swing.JFrame;
+
+
+
+
+/**
+ * This class contains the server side gui code. It is written in Java Swing.
+ * @author jmcelroy
+ *
+ */
+public class ServerGUI extends JFrame {
+
+	
+	
+	public ServerGUI(){
+		this.setTitle("PineApple Server");
+		
+		
+	}
+	
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
