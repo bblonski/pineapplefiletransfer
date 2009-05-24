1a081afda4ec9143f4e1fb18fe20dbaf86f32b48
bblonski <bblonski@73532c6c-3e55-11de-be05-5f7a86268029>
2009-05-24 03:21:17 +0000

    Implemented FileList class and added associated test.

--- trunk/server/src/com/pineapple/FileList.java
+++ trunk/server/src/com/pineapple/FileList.java
@@ -21,5 +21,23 @@ public class FileList
     {
         return list.remove(file);
     }
+    
+    public int size()
+    {
+        return list.size();
+    }
+    
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        for(FileUpdate fu: list)
+        {
+            sb.append(fu.toString());
+            /*I think we can do Unix only newlines here because we open the file
+            on Unix machines only.*/
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 
 }
