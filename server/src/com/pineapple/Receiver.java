8bdadf1a25e2351791232f04eea6c2f6d0ad8dfe
gmgire <gmgire@73532c6c-3e55-11de-be05-5f7a86268029>
2009-05-28 20:21:00 +0000

    Added Receiver.java class that is meant to receive messages from Client and store client's address. Transmitter needs to be initialized with server's IP address (not sure how to correctly get this; don't want local host IP 127.0.0.1).

---
+++ server/src/com/pineapple/Receiver.java
@@ -0,0 +1,18 @@
+package com.pineapple;
+
+public class Receiver {
+
+	private String clientAddress;
+	
+	public Receiver() {
+		this.clientAddress = null;
+	}
+	
+	public String getClientAddress() {
+		return this.clientAddress;
+	}
+	
+	public void setClientAddress(String clientAddress) {
+		this.clientAddress = clientAddress;
+	}
+}
